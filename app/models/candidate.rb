b126de8359ea05bca429a0b413cf2958ccee1d38
Becky Boone <becky.sue.boone@gmail.com>
2015-07-01 14:25:11 -0600

    Add candidate view
    Add routes, controller, and views. Make dev pretty

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,6 @@
 class Candidate < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+
   validates :name, presence: true
   validates :photo_url, format: URI.regexp, allow_nil: true
   validates :website, format: URI.regexp, allow_nil: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
   post "/", to: "home#results"
+  resources :candidates
 end
